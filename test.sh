71d9def9aac0707e91a0b3ee69dec715930fda39
Frank Lyaruu <frank@dexels.com>
2019-05-11 12:48:31 +0200

    added test script

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+curl http://localhost:8080/adba-r2dbc-demo/Blocking
+curl http://localhost:8080/adba-r2dbc-demo/SQLServletR2DBC
+curl http://localhost:8080/adba-r2dbc-demo/SQLServletSimpleADBABlocking
+curl http://localhost:8080/adba-r2dbc-demo/SQLServletADBABlocking
+curl http://localhost:8080/adba-r2dbc-demo/SQLServletADBANonBlocking
+curl http://localhost:8080/adba-r2dbc-demo/SQLServletADBANonBlockingBackpressure
+curl http://localhost:8080/adba-r2dbc-demo/SQLServletR2DBC
+
