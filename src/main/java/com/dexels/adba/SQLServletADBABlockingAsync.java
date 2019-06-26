d6c5086def3e0783ed5083cb40a3ff91668e8481
Frank Lyaruu <frank@dexels.com>
2019-06-26 08:52:28 +0200

    added async / blocking ADBA version

---
+++ src/main/java/com/dexels/adba/SQLServletADBABlockingAsync.java
@@ -0,0 +1,51 @@
+package com.dexels.adba;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import jdk.incubator.sql2.DataSource;
+import jdk.incubator.sql2.DataSourceFactory;
+import jdk.incubator.sql2.Session;
+
+public class SQLServletADBABlockingAsync extends HttpServlet {
+
+	private static final long serialVersionUID = 4008686226298740688L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException {
+		DataSource ds = DataSourceFactory.newFactory("org.postgresql.adba.PgDataSourceFactory")
+				.builder()
+				.url("jdbc:postgresql://postgres:5432/dvdrental")
+				.username("postgres")
+				.password("mysecretpassword")
+				.build();
+ 
+		AsyncContext context = req.startAsync();
+		context.setTimeout(60000);
+		try (Session session = ds.getSession()) {
+			Writer outputWriter = resp.getWriter();
+			session.<Writer>rowOperation("select title from film")
+					.collect(() -> outputWriter, (writer, row) -> {
+						try {
+							writer.write(row.at("title").get(String.class)+"\n");
+						} catch (IOException e) {}
+					})
+					.submit()
+					.getCompletionStage()
+					.toCompletableFuture()
+					.thenAccept(w->context.complete());
+		} catch (IOException e) {
+			throw new ServletException("whoops", e);
+		}
+	}
+
+}
--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ curl http://localhost:8080/adba-r2dbc-demo/Blocking
 curl http://localhost:8080/adba-r2dbc-demo/SQLServletR2DBC
 curl http://localhost:8080/adba-r2dbc-demo/SQLServletSimpleADBABlocking
 curl http://localhost:8080/adba-r2dbc-demo/SQLServletADBABlocking
+curl http://localhost:8080/adba-r2dbc-demo/SQLServletADBABlockingAsync
 curl http://localhost:8080/adba-r2dbc-demo/SQLServletADBANonBlocking
 curl http://localhost:8080/adba-r2dbc-demo/SQLServletADBANonBlockingBackpressure
 curl http://localhost:8080/adba-r2dbc-demo/SQLServletR2DBC
-
